6b40e6254f1a6ca35220372796cc0af851e1afe4
TillyThomas <tillythomas100@icloud.com>
2026-08-31 17:31:23 -0400

    Remove download and save instructions from Day 02.R
    Removed instructions for downloading and saving the file.

--- Real Data D02.R
+++ Real Data D02.R
@@ -1,10 +1,3 @@
-## >>> GET & SAVE (details in the R guide) -------------------------
-## GET, git mode  -> run in the Console:
-##   download.file("https://raw.githubusercontent.com/isaaccloh/ECON377/main/377_2026/D02/D02_starter.R", "D02.R")
-## GET, easy mode -> copy this file from github.com/isaaccloh/ECON377 (377_2026/D02) into a new script
-## SAVE your work -> commit + push D02.R to your own econ377 repo (or upload it on github.com)
-## ----------------------------------------------------------------
-
 ## ECN 377 - Day 2  |  first R session: the mean, real data, a first line.
 
 ## (1) The mean, from our own numbers  -- we fill this in together
